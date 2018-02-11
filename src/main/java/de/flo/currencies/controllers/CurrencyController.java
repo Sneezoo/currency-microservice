02ff5d28ea8a237d61fff9c9f5968076088e7258
Sneezoo <blue.xdimension@gmail.com>
2018-02-11 21:51:08 +0100

    Add CurrencyController

---
+++ src/main/java/de/flo/currencies/controllers/CurrencyController.java
@@ -0,0 +1,31 @@
+package de.flo.currencies.controllers;
+
+import de.flo.currencies.model.CurrencyRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CurrencyController {
+
+    @Resource
+    CurrencyRepository currencyRepository;
+
+    @RequestMapping("/currencies")
+    public List<String> currencies() {
+        return currencyRepository.getAllCurrencies();
+    }
+
+    @RequestMapping("/currencies/{symbol}")
+    public double currency(@PathVariable("symbol") String symbol) {
+        Double value = currencyRepository.getBySymbol(symbol);
+        if (value == null) {
+            throw new IllegalArgumentException("Currency " + symbol + " is not available");
+        }
+        return value;
+    }
+}
