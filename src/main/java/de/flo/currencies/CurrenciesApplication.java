1b494b723de497ba3dc9ff2306a8b9bb2d6c4c6c
Florian Hoyer <f.hoyer@hoffmann-group.com>
2018-02-11 17:42:33 +0100

    Project setup

---
+++ src/main/java/de/flo/currencies/CurrenciesApplication.java
@@ -0,0 +1,12 @@
+package de.flo.currencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrenciesApplication.class, args);
+	}
+}
